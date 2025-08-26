fd1e2f960732ec51927c35ed5838d41df29e43e9
Zezzzzz <g.u.a.nze95@gmail.com>
2025-08-25 22:19:56 -0700

    Committing repo name for config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base = '/ilp_app/'
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base,
 })
